1b765198b3ebfdf5e7340c642563fa92076d434b
digidikari <digidikari@gmail.com>
2025-03-18 10:57:37 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,8 @@
+function logout() {
+    auth.signOut().then(() => {
+        alert("Logout berhasil!");
+        window.location.href = "index.html";
+    }).catch(error => alert(error.message));
+}
 
+window.logout = logout;
